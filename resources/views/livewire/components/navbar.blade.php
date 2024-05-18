7fc1737bc336e55b6557885b53a46dc5e2f7ed96
ufik0203@gmail.com <ufik0203@gmail.com>
2024-05-19 03:31:31 +0700

    update commit

--- resources/views/livewire/components/navbar.blade.php
+++ resources/views/livewire/components/navbar.blade.php
@@ -74,7 +74,7 @@
           </div>
         </a>
         <a class="font-medium text-slate-600 hover:text-gray-400" href="#">Product</a>
-        <a class="font-medium text-slate-600 hover:text-gray-400" href="#">About</a>
+        <a class="font-medium text-slate-600 hover:text-gray-400" href="/About" wire:navigate>About</a>
         <a class="font-medium text-slate-600 hover:text-gray-400" href="/contact" wire:navigate>Contact</a>
         <a class="font-medium text-slate-600 hover:text-gray-400 lg:hidden" href="/login" wire:navigate>Login</a>
       </div>
---
+++ resources/views/livewire/pages/about.blade.php
@@ -0,0 +1,174 @@
+{{-- About section hero 1 start --}}
+<div>
+
+    <section>
+        <div class="w-10/12 mx-auto md:grid grid-cols-2 min-h-[500px]">
+            <div class="grid">
+                <div class="items-center text-center md:text-start my-auto">
+                    <p class="font-bold text-2xl my-10">
+                        ABOUT COMPANY
+                    </p>
+                    <p class="font-semibold text-8xl mx-auto my-auto">
+                        ABOUT US
+                    </p>
+                    <p class="text-2xl my-10">
+                        <span class="inline-block">We know how large objects will act, but things on</span>
+                        <span class="block">a small scale are quite different.</span>
+                    </p>
+                    <a href=""
+                        class="inline-block px-8 py-3 font-semibold mb-20 rounded-md bg-blue-500 text-white">Get
+                        Quote
+                        Now</a>
+                </div>
+            </div>
+            <div class="lg:justify-self-end">
+                <img src="/img/about-hero-1.png" alt="" class="my-auto">
+            </div>
+        </div>
+    </section>
+    {{-- About section hero 1 end --}}
+
+    {{-- No section start --}}
+    <section>
+        <div class="container w-10/12 mx-auto grid grid-rows-2 md:grid-cols-2 min-h-[200px] my-20 justify-items-center">
+            <div class="grid text-center md:text-start mx-auto px-16">
+                <p class="font-semibold text-red-500 text-xl">
+                    Problems trying
+                </p>
+                <p class="text-4xl font-bold mt-5">
+                    Met minim Mollie non desert Alamo est sit cliquery dolor do met sent.
+                </p>
+            </div>
+            <div class="grid items-center px-16">
+                <p class="text-lg">
+                    Problems trying to resolve the conflict between the two major realsm of Classical physichs:
+                    Newtonian mechanics.
+                </p>
+            </div>
+        </div>
+    </section>
+    {{-- No section end --}}
+
+    {{-- Count section start --}}
+    <section>
+        <div class="container w-10/12 mx-auto min-h-[200px] text-center content-center pb-10 mb-10">
+            <div class="grid md:grid-cols-2 lg:grid-cols-4 font-semibold text-8xl text-center">
+                <p class="inline-block pt-10">15K <span class="text-base block font-semibold">Happy Customers</span></p>
+                <p class="inline-block pt-10">150K <span class="text-base block font-semibold">Monthly Visitors</span>
+                </p>
+                <p class="inline-block pt-10">15 <span class="text-base block font-semibold">Countries Worldwide</span>
+                </p>
+                <p class="inline-block pt-10">100+ <span class="text-base block font-semibold">Top Partners</span></p>
+            </div>
+        </div>
+    </section>
+    {{-- count section end --}}
+
+    {{-- Video section start --}}
+    <section>
+        <div
+            class="container w-8/12 mx-auto min-h-[500px] grid grid-cols-1 content-center justify-items-center py-20 my-20">
+            <div class="h-full w-full mx-auto">
+                <iframe class="min-h-96 min-w-80 lg:min-h-[500px] sm:min-w-96 w-full rounded-xl mx-auto"
+                    src="https://www.youtube.com/embed/mNzenfB3OuY?si=XZEsazcxBv7dMCt6" title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </div>
+    </section>
+    {{-- Video section end --}}
+
+    {{-- Teams section start --}}
+    <section>
+        <div class="container w-10/12 mx-auto lg:min-h-[500px] my-20 py-20">
+            <div class="grid text-center justify-center items-center mb-24">
+                <p class="inline-block font-semibold text-6xl">
+                    Meet Our Team
+                    <span class="block font-normal text-xl pt-10 tracking-wider">
+                        Problems trying resolve the conflict between
+                    </span>
+                    <span class="block font-normal text-xl">
+                        the two major realms of Classical physics: Newtonian mechanics.
+                    </span>
+                </p>
+            </div>
+            <div class="grid sm:grid-cols-2 lg:grid-cols-4 justify-items-center content-center">
+                <div class="grid grid-rows-2 mt-12">
+                    <div class="w-full h-full flex justify-center">
+                        <img src="/img/user/user-1.png" alt="" class="rounded-full w-36 h-36">
+                    </div>
+                    <div class="mt-2 text-center max-w-44">
+                        <p class="font-bold text-sky-500">Programmer
+                            <span class="block mt-5 text-xl text-black">Irfan Yasin</span>
+                            <span class="block mt-5 font-normal text-black">the quick fox jumps over the lazy dog</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="grid grid-rows-2 mt-12">
+                    <div class="w-full h-full flex justify-center">
+                        <img src="/img/user/user-2.png" alt="" class="rounded-full w-36 h-36">
+                    </div>
+                    <div class="mt-2 text-center max-w-44">
+                        <p class="font-bold text-sky-500">Crypto Developer
+                            <span class="block mt-5 text-xl text-black">Mario Achmad T</span>
+                            <span class="block mt-5 font-normal text-black">the quick fox jumps over the lazy dog</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="grid grid-rows-2 mt-12">
+                    <div class="w-full h-full flex justify-center">
+                        <img src="/img/user/user-3.png" alt="" class="rounded-full w-36 h-36">
+                    </div>
+                    <div class="mt-2 text-center max-w-44">
+                        <p class="font-bold text-sky-500">AI Enginer
+                            <span class="block mt-5 text-xl text-black">Raihan Firdaus</span>
+                            <span class="block mt-5 font-normal text-black">the quick fox jumps over the lazy dog</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="grid grid-rows-2 mt-12">
+                    <div class="w-full h-full flex justify-center">
+                        <img src="/img/user/user-4.png" alt="" class="rounded-full w-36 h-36">
+                    </div>
+                    <div class="mt-2 text-center max-w-44">
+                        <p class="font-bold text-sky-500">Data Mining
+                            <span class="block mt-5 text-xl text-black">Amoure C.D.P</span>
+                            <span class="block mt-5 font-normal text-black">the quick fox jumps over the lazy dog</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+    </section>
+    {{-- Teams section end --}}
+
+    {{-- About section hero-2 start --}}
+    <section class="bg-blue-500 grid lg:grid-cols-3">
+        <div class="lg:col-span-2 my-24">
+            <div class="grid lg:grid-cols-3 w-10/12 min-h-[500px] mx-auto">
+                <div class="col-span-2 text-white flex items-center text-center md:text-start">
+                    <p class="font-bold text-xl max-w-2xl">
+                        WORK WITH US
+                        <span class="block text-6xl my-12">
+                            Now Let's grow Yours
+                        </span>
+                        <span class="block font-normal">
+                            The gradual accumulation of information about atomic and small-scale berhavior
+                            during the first quarter of the 20th
+                        </span>
+                        <span>
+                            <a href=""
+                                class="inline-block px-8 py-4 font-bold uppercase border rounded-lg text-white border-white mt-10">Read
+                                More</a>
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="hidden xl:inline-block">
+            <img src="/img/about-hero-2.png" alt="" class="h-full w-full">
+        </div>
+    </section>
+    {{-- About section hero-2 end --}}
+
+</div>

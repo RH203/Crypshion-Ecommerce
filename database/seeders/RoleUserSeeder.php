3aaa88f76e8d6a1a5120090077707d6031220730
alirfanyasin <risayunita21@gmail.com>
2024-05-20 15:43:04 +0700

    feat: boilerplate authentication

---
+++ database/seeders/RoleUserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class RoleUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Admin
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $admin->assignRole('admin');
+
+
+        // User
+        $user = User::create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $user->assignRole('user');
+    }
+}

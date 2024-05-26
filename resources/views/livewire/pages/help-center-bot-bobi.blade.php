3bd25a86d1753869dbbccb379df87d147bbe06df
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-05-26 21:09:54 +0700

    feat: Update error code link in HelpCenter page

--- resources/views/livewire/pages/help-center-bot-bobi.blade.php
+++ resources/views/livewire/pages/help-center-bot-bobi.blade.php
@@ -1,3 +1,124 @@
 <div>
-    Bobi
+  <div class="w-10/12 mx-auto flex flex-col">
+    {{-- Title Start --}}
+    <div class="w-full flex justify-center items-center mt-4">
+      <p class="text-5xl text-blueNavy font-semibold">Ask Bobi</p>
+    </div>
+    <hr class="border-gray-800 mt-4">
+    {{-- Title End --}}
+    <div
+      class="mx-auto mt-10 h-96 w-4/5  bg-gray-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100 p-10 overflow-auto ">
+      <!-- Chat Bubble -->
+      <ul class="space-y-5">
+        <!-- Chat -->
+        <li class="max-w-lg flex gap-x-2 sm:gap-x-4">
+          <!-- Card -->
+          <div
+            class="bg-white border border-gray-200 rounded-2xl p-4 space-y-3 dark:bg-neutral-900 dark:border-neutral-700">
+            <h2 class="font-medium text-gray-800 dark:text-white">
+              How can we help?
+            </h2>
+            <div class="space-y-1.5">
+              <p class="mb-1.5 text-sm text-gray-800 dark:text-white">
+                You can ask questions like:
+              </p>
+              <ul class="list-disc list-outside space-y-1.5 ps-3.5">
+                <li class="text-sm text-gray-800 dark:text-white">
+                  What's Preline UI?
+                </li>
+
+                <li class="text-sm text-gray-800 dark:text-white">
+                  How many Starter Pages & Examples are there?
+                </li>
+
+                <li class="text-sm text-gray-800 dark:text-white">
+                  Is there a PRO version?
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- End Card -->
+        </li>
+        <!-- End Chat -->
+
+        <!-- Chat -->
+        <li class="max-w-lg ms-auto flex justify-end gap-x-2 sm:gap-x-4">
+          <div class="grow text-end space-y-3">
+            <!-- Card -->
+            <div class="inline-block bg-blue-600 rounded-2xl p-4 shadow-sm">
+              <p class="text-sm text-white">
+                what's preline ui?
+              </p>
+            </div>
+            <!-- End Card -->
+          </div>
+        </li>
+        <!-- End Chat -->
+
+        <!-- Chat Bubble -->
+        <li class="max-w-lg flex gap-x-2 sm:gap-x-4">
+          <!-- Card -->
+          <div
+            class="bg-white border border-gray-200 rounded-2xl p-4 space-y-3 dark:bg-neutral-900 dark:border-neutral-700">
+            <p class="text-sm text-gray-800 dark:text-white">
+              Preline UI is an open-source set of prebuilt UI components based on the utility-first Tailwind CSS
+              framework.
+            </p>
+            <div class="space-y-1.5">
+              <p class="text-sm text-gray-800 dark:text-white">
+                Here're some links to get started
+              </p>
+              <ul>
+                <li>
+                  <a class="text-sm text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500 dark:hover:text-blue-400"
+                    href="../docs/index.html">
+                    Installation Guide
+                  </a>
+                </li>
+                <li>
+                  <a class="text-sm text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500 dark:hover:text-blue-400"
+                    href="../docs/frameworks.html">
+                    Framework Guides
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- End Card -->
+        </li>
+        <!-- End Chat Bubble -->
+      </ul>
+      <!-- End Chat Bubble -->
+    </div>
+
+    {{-- Form Start --}}
+    <div class="mx-auto mb-10 shadow-sm w-4/5">
+      <div class="relative flex rounded-lg shadow-sm">
+        <input type="text" id="hs-search-box-with-loading-5" name="hs-search-box-with-loading-5"
+          class="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm rounded-s-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+          placeholder="Input search">
+        <div class="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-4">
+          <svg class="flex-shrink-0 size-4 text-gray-400 dark:text-neutral-500" xmlns="http://www.w3.org/2000/svg"
+            width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+            stroke-linecap="round" stroke-linejoin="round">
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </svg>
+        </div>
+        <button type="button"
+          class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-e-md border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+          <span
+            class="animate-spin inline-block size-4 border-[3px] border-current border-t-transparent text-white rounded-full"
+            role="status" aria-label="loading">
+            <span class="sr-only">Loading...</span>
+          </span>
+          Search
+        </button>
+      </div>
+    </div>
+  </div>
+
 </div>
+
+@script
+@endscript
--- resources/views/livewire/pages/help-center-error-code.blade.php
+++ resources/views/livewire/pages/help-center-error-code.blade.php
@@ -8,14 +8,13 @@
     {{-- Title End --}}
 
     {{-- Accordion Start --}}
-
     <div class="w-full mt-10">
       <div class="hs-accordion-group">
         <div
-          class="hs-accordion active bg-white border -mt-px first:rounded-t-lg last:rounded-b-lg dark:bg-neutral-800 dark:border-neutral-700"
+          class="hs-accordion active hs-accordion-active:bg-transparent border-2 -mt-px first:rounded-t-lg last:rounded-b-lg"
           id="hs-bordered-heading-one">
           <button
-            class="hs-accordion-toggle hs-accordion-active:text-blue-600 inline-flex items-center gap-x-3 w-full font-semibold text-start text-gray-800 py-4 px-5 hover:text-gray-500 disabled:opacity-50 disabled:pointer-events-none dark:hs-accordion-active:text-blue-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:outline-none dark:focus:text-neutral-400"
+            class="hs-accordion-toggle hs-accordion-active:bg-transparent inline-flex items-center gap-x-3 w-full font-semibold text-start text-blueNavy py-4 px-5 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none "
             aria-controls="hs-basic-bordered-collapse-one">
             <svg class="hs-accordion-active:hidden block size-3.5" xmlns="http://www.w3.org/2000/svg" width="24"
               height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
@@ -33,8 +32,8 @@
           <div id="hs-basic-bordered-collapse-one"
             class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
             aria-labelledby="hs-bordered-heading-one">
-            <div class="pb-4 px-5">
-              <p class="text-gray-800 dark:text-neutral-200">
+            <div class="pb-4 px-5 ">
+              <p class="text-blueNavy">
                 <em>This is the third item's accordion body.</em> It is hidden by default, until the collapse plugin
                 adds the appropriate classes that we use to style each element. These classes control the overall
                 appearance, as well as the showing and hiding via CSS transitions.
@@ -44,11 +43,11 @@
         </div>
 
         <div
-          class="hs-accordion bg-white border -mt-px first:rounded-t-lg last:rounded-b-lg dark:bg-neutral-800 dark:border-neutral-700"
-          id="hs-bordered-heading-two">
+          class="hs-accordion active hs-accordion-active:bg-transparent border-2 -mt-px first:rounded-t-lg last:rounded-b-lg"
+          id="hs-bordered-heading-one">
           <button
-            class="hs-accordion-toggle hs-accordion-active:text-blue-600 inline-flex items-center gap-x-3 w-full font-semibold text-start text-gray-800 py-4 px-5 hover:text-gray-500 disabled:opacity-50 disabled:pointer-events-none dark:hs-accordion-active:text-blue-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:outline-none dark:focus:text-neutral-400"
-            aria-controls="hs-basic-bordered-collapse-two">
+            class="hs-accordion-toggle hs-accordion-active:bg-transparent inline-flex items-center gap-x-3 w-full font-semibold text-start text-blueNavy py-4 px-5 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none "
+            aria-controls="hs-basic-bordered-collapse-one">
             <svg class="hs-accordion-active:hidden block size-3.5" xmlns="http://www.w3.org/2000/svg" width="24"
               height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
               stroke-linecap="round" stroke-linejoin="round">
@@ -60,14 +59,14 @@
               stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
             </svg>
-            Accordion #2
+            Accordion #1
           </button>
-          <div id="hs-basic-bordered-collapse-two"
-            class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
-            aria-labelledby="hs-bordered-heading-two">
-            <div class="pb-4 px-5">
-              <p class="text-gray-800 dark:text-neutral-200">
-                <em>This is the second item's accordion body.</em> It is hidden by default, until the collapse plugin
+          <div id="hs-basic-bordered-collapse-one"
+            class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
+            aria-labelledby="hs-bordered-heading-one">
+            <div class="pb-4 px-5 ">
+              <p class="text-blueNavy">
+                <em>This is the third item's accordion body.</em> It is hidden by default, until the collapse plugin
                 adds the appropriate classes that we use to style each element. These classes control the overall
                 appearance, as well as the showing and hiding via CSS transitions.
               </p>
@@ -76,11 +75,11 @@
         </div>
 
         <div
-          class="hs-accordion bg-white border -mt-px first:rounded-t-lg last:rounded-b-lg dark:bg-neutral-800 dark:border-neutral-700"
-          id="hs-bordered-heading-three">
+          class="hs-accordion active hs-accordion-active:bg-transparent border-2 -mt-px first:rounded-t-lg last:rounded-b-lg"
+          id="hs-bordered-heading-one">
           <button
-            class="hs-accordion-toggle hs-accordion-active:text-blue-600 inline-flex items-center gap-x-3 w-full font-semibold text-start text-gray-800 py-4 px-5 hover:text-gray-500 disabled:opacity-50 disabled:pointer-events-none dark:hs-accordion-active:text-blue-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:outline-none dark:focus:text-neutral-400"
-            aria-controls="hs-basic-bordered-collapse-three">
+            class="hs-accordion-toggle hs-accordion-active:bg-transparent inline-flex items-center gap-x-3 w-full font-semibold text-start text-blueNavy py-4 px-5 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none "
+            aria-controls="hs-basic-bordered-collapse-one">
             <svg class="hs-accordion-active:hidden block size-3.5" xmlns="http://www.w3.org/2000/svg" width="24"
               height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
               stroke-linecap="round" stroke-linejoin="round">
@@ -92,14 +91,14 @@
               stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
             </svg>
-            Accordion #3
+            Accordion #1
           </button>
-          <div id="hs-basic-bordered-collapse-three"
-            class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
-            aria-labelledby="hs-bordered-heading-three">
-            <div class="pb-4 px-5">
-              <p class="text-gray-800 dark:text-neutral-200">
-                <em>This is the first item's accordion body.</em> It is hidden by default, until the collapse plugin
+          <div id="hs-basic-bordered-collapse-one"
+            class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
+            aria-labelledby="hs-bordered-heading-one">
+            <div class="pb-4 px-5 ">
+              <p class="text-blueNavy">
+                <em>This is the third item's accordion body.</em> It is hidden by default, until the collapse plugin
                 adds the appropriate classes that we use to style each element. These classes control the overall
                 appearance, as well as the showing and hiding via CSS transitions.
               </p>

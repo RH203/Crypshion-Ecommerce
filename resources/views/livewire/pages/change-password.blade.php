840c2a7c9e79da5bc975c2ac35f92fab1cbf814c
alirfanyasin <risayunita21@gmail.com>
2024-05-28 20:36:35 +0700

    feat: change password

--- resources/views/livewire/pages/change-password.blade.php
+++ resources/views/livewire/pages/change-password.blade.php
@@ -5,24 +5,39 @@
         <h1 class="text-xl font-semibold">Change Password</h1>
       </header>
       <div class="">
-        <form action="" class="block gap-5 ">
+        <form wire:submit.prevent='changePassword' class="block gap-5 ">
           <div class="text-start">
             <div class="mb-3">
-              <label for="name" class="text-sm text-slate-500">Current Password</label>
-              <input type="text" name="name" id="name" class="w-full px-3 py-1 border rounded-md">
+              <label for="currentPassword" class="text-sm text-slate-500">Current Password</label>
+              <input type="password" wire:model='currentPassword' name="currentPassword" id="currentPassword"
+                class="w-full px-3 py-1 border rounded-md">
+              @error('currentPassword')
+                <small class="italic text-red-600"><iconify-icon icon="quill:warning-alt"></iconify-icon>
+                  {{ $message }}</small>
+              @enderror
             </div>
             <div class="mb-3">
-              <label for="name" class="text-sm text-slate-500">New Password</label>
-              <input type="text" name="name" id="name" class="w-full px-3 py-1 border rounded-md">
+              <label for="newPassword" class="text-sm text-slate-500">New Password</label>
+              <input type="password" wire:model='newPassword' name="newPassword" id="newPassword"
+                class="w-full px-3 py-1 border rounded-md">
+              @error('newPassword')
+                <small class="italic text-red-600"><iconify-icon icon="quill:warning-alt"></iconify-icon>
+                  {{ $message }}</small>
+              @enderror
             </div>
             <div class="mb-3">
-              <label for="name" class="text-sm text-slate-500">Confirm Password</label>
-              <input type="text" name="name" id="name" class="w-full px-3 py-1 border rounded-md">
+              <label for="confirmPassword" class="text-sm text-slate-500">Confirm Password</label>
+              <input type="password" name="confirmPassword" wire:model='confirmPassword' id="confirmPassword"
+                class="w-full px-3 py-1 border rounded-md">
+              @error('confirmPassword')
+                <small class="italic text-red-600"><iconify-icon icon="quill:warning-alt"></iconify-icon>
+                  {{ $message }}</small>
+              @enderror
             </div>
             <div class="mb-3">
-              <button class="px-8 py-2 text-sm font-normal text-white rounded-md bg-primaryBg ">Save</button>
+              <button type="submit"
+                class="px-8 py-2 text-sm font-normal text-white rounded-md bg-primaryBg ">Save</button>
             </div>
-
           </div>
         </form>
       </div>

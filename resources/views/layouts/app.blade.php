29e0d2572aaeb4039b935492799128af7bd3096f
alirfanyasin <risayunita21@gmail.com>
2024-05-31 21:32:52 +0700

    feat: integrataion midtrans

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,11 @@
   {{-- JQuery --}}
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
+  {{-- Midtrans --}}
+  <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js"
+    data-client-key="{{ env('MIDTRANS_CLIENT_KEY') }}"></script>
+
+
   {{-- Vite --}}
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>

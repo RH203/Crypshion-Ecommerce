f1cf3ee20686586095bb8e8c9914db51c0ad8940
alirfanyasin <risayunita21@gmail.com>
2024-06-03 21:55:32 +0700

    feat: searching

--- app/Livewire/Pages/Product.php
+++ app/Livewire/Pages/Product.php
@@ -5,47 +5,71 @@ namespace App\Livewire\Pages;
 use App\Models\app\Product as AppProduct;
 use App\Models\Order;
 use App\Models\Rating;
-use App\Trait\Products;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 
 class Product extends Component
 {
-    use Products;
-
     public $products;
 
     #[Url()]
-    public $search = '';
+    public $search;
 
     public function mount()
     {
-        $this->products = $this->getProducts(null);
+        // Inisialisasi produk tanpa mempertimbangkan pencarian
+        $this->resetSearch();
+    }
+
+    public function resetSearch()
+    {
+        // Mendapatkan semua produk tanpa mempertimbangkan pencarian
+        $this->products = AppProduct::orderBy('id', 'desc')
+            ->get()
+            ->map(function ($product) {
+                $product->images = json_decode($product->images, true);
+                $product->prices = json_decode($product->prices, true);
+                $sizes = array_keys($product->prices);
+                $product->first_image = $product->images[0] ?? null;
+                $product->first_price = $product->prices[$sizes[0]] ?? null;
+                return $product;
+            });
     }
 
     public function searchProduct()
     {
-        $filteredProducts = AppProduct::latest()
-            ->where('title', 'like', "%{$this->search}%")
-            ->get();
-
-        return $filteredProducts;
+        if ($this->search) {
+            // Memfilter produk berdasarkan pencarian yang sesuai
+            $this->products = AppProduct::orderBy('id', 'desc')
+                ->where('title', 'like', '%' . $this->search . '%')
+                ->get()
+                ->map(function ($product) {
+                    $product->images = json_decode($product->images, true);
+                    $product->prices = json_decode($product->prices, true);
+                    $sizes = array_keys($product->prices);
+                    $product->first_image = $product->images[0] ?? null;
+                    $product->first_price = $product->prices[$sizes[0]] ?? null;
+                    return $product;
+                });
+        } else {
+            $this->resetSearch();
+        }
     }
 
     public function render()
     {
-        $filteredProducts = $this->searchProduct();
+        $this->searchProduct();
 
         $averageRatings = [];
         $soldQuantities = [];
 
-        foreach ($filteredProducts as $product) {
+        foreach ($this->products as $product) {
             $averageRatings[$product->id] = Rating::where('product_id', $product->id)->avg('rating');
             $soldQuantities[$product->id] = Order::where('product_id', $product->id)->sum('quantity');
         }
 
         return view('livewire.pages.product', [
-            'products' => $filteredProducts,
+            'products' => $this->products,
             'averageRatings' => $averageRatings,
             'soldQuantities' => $soldQuantities,
         ]);

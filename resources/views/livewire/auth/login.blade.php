fa4009e76d7132d68cbb9384efeeeea1b793d5db
alirfanyasin <risayunita21@gmail.com>
2024-06-04 10:33:53 +0700

    feat: login with google

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -7,9 +7,9 @@
        </div>
      </div>
    @enderror
-   <div class="mb-10">
+   <div class="mb-5">
      <input type="email" name="name" wire:model='email'
-       class="w-full px-5 py-3 border-[#23A6F0] border-b-2 bg-transparent rounded-xl placeholder:font-light placeholder:text-slate-400 focus:outline-0"
+       class="w-full px-5 py-3 bg-white border shadow-lg border-primary rounded-xl placeholder:font-light placeholder:text-slate-400 focus:outline-0"
        placeholder="Email Address">
      @error('email')
        <small class="italic text-red-600"><iconify-icon icon="quill:warning-alt"></iconify-icon>
@@ -18,7 +18,7 @@
    </div>
    <div class="mb-5">
      <input type="password" name="password" wire:model='password'
-       class="w-full px-5 py-3 border-[#23A6F0] border-b-2 bg-transparent rounded-xl placeholder:font-light placeholder:text-slate-400 focus:outline-0"
+       class="w-full px-5 py-3 bg-white border shadow-lg border-primary rounded-xl placeholder:font-light placeholder:text-slate-400 focus:outline-0"
        placeholder="Password">
      @error('password')
        <small class="italic text-red-600"><iconify-icon icon="quill:warning-alt"></iconify-icon>
@@ -29,7 +29,8 @@
      <a href="" class="hover:text-[#23A6F0] hover:font-semibold text-black">Forgot Password?</a>
    </div>
    <div class="mb-5">
-     <button type="submit" class="h-14 w-full rounded-lg font-bold text-white text-xl bg-[#23A6F0]">Sign In</button>
+     <button type="submit" class="h-14 shadow-lg w-full rounded-lg font-bold text-white text-xl bg-[#23A6F0]">Sign
+       In</button>
    </div>
    <div class="text-center">
      <span>Don't have an account? <a href="/register" wire:navigate class="text-[#23A6F0] font-bold">Sign

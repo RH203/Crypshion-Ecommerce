7788f04df758f06faadce3512ab4fd4aad1a9eb3
amourepurba <amourechelpurba@gmail.com>
2024-06-05 21:40:36 +0700

    Update Help Center

--- resources/views/livewire/pages/help-center-delivery.blade.php
+++ resources/views/livewire/pages/help-center-delivery.blade.php
@@ -7,7 +7,7 @@
     <hr class="border-gray-800 mt-4">
     {{-- Title End --}}
 
-    {{-- Accordion Start --}}
+  {{-- Accordion Start --}}
     <div class="w-full mt-10">
       <div class="hs-accordion-group">
         <div
@@ -27,21 +27,41 @@
               stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
             </svg>
-            Accordion #1
+            Delivery Service
           </button>
           <div id="hs-basic-bordered-collapse-one"
             class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
             aria-labelledby="hs-bordered-heading-one">
             <div class="pb-4 px-5 ">
               <p class="text-blueNavy">
-                <em>This is the third item's accordion body.</em> It is hidden by default, until the collapse plugin
-                adds the appropriate classes that we use to style each element. These classes control the overall
-                appearance, as well as the showing and hiding via CSS transitions.
+                Jenis pengiriman apa saja yang tersedia?
+              <ul class="marker:text-blue-600 list-disc ps-5 space-y-2 text-sm text-gray-600 mt-3">
+                <li>
+                Kami menawarkan berbagai jenis layanan pengiriman
+                </li>
+                <li>
+                Pengiriman domestik,
+                Pengiriman internasional,
+                Pengiriman ekspresi,
+                Pengiriman ekonomis.
+                </li>
+                <li class="">
+                  Jika Anda masih mengalami masalah, silakan hubungi tim support kami melalui link berikut
+                  <div class="flex gap-2">
+                    <p><a class="text-blue-600 hover:text-blue-500 opacity-90"
+                        href="https://wa.me/+6282229878219">Whatapp</a></p>
+                    <p><a class="text-blue-600 hover:text-blue-500 opacity-90"
+                        href="mailto:example@example.com?subject=Subject of the Email&body=Hello, this is the body of the email.">Email</a>
+                    </p>
+                  </div>
+                </li>
+              </ul>
               </p>
             </div>
           </div>
         </div>
 
+        {{-- Track Shipment Start --}}
         <div
           class="hs-accordion active hs-accordion-active:bg-transparent border-2 -mt-px first:rounded-t-lg last:rounded-b-lg"
           id="hs-bordered-heading-one">
@@ -59,21 +79,39 @@
               stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
             </svg>
-            Accordion #1
+            Track Shipment
           </button>
           <div id="hs-basic-bordered-collapse-one"
             class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
             aria-labelledby="hs-bordered-heading-one">
             <div class="pb-4 px-5 ">
               <p class="text-blueNavy">
-                <em>This is the third item's accordion body.</em> It is hidden by default, until the collapse plugin
-                adds the appropriate classes that we use to style each element. These classes control the overall
-                appearance, as well as the showing and hiding via CSS transitions.
+                Bagaimana cara melacak kiriman saya?
+              <ul class="marker:text-blue-600 list-disc ps-5 space-y-2 text-sm text-gray-600 mt-3">
+                <li>
+                Anda dapat melacak kiriman Anda menggunakan nomor resi yang Anda terima saat melakukan pemesanan
+                </li>
+                <li>
+                Anda dapat melacak kiriman Anda di website kami.
+                </li>
+                <li class="">
+                  Jika Anda masih mengalami masalah, silakan hubungi tim support kami melalui link berikut
+                  <div class="flex gap-2">
+                    <p><a class="text-blue-600 hover:text-blue-500 opacity-90"
+                        href="https://wa.me/+6282229878219">Whatapp</a></p>
+                    <p><a class="text-blue-600 hover:text-blue-500 opacity-90"
+                        href="mailto:example@example.com?subject=Subject of the Email&body=Hello, this is the body of the email.">Email</a>
+                    </p>
+                  </div>
+                </li>
+              </ul>
               </p>
             </div>
           </div>
         </div>
+        {{-- Track Shipment End --}}
 
+        {{-- Damaged Lost Shipment Start --}}
         <div
           class="hs-accordion active hs-accordion-active:bg-transparent border-2 -mt-px first:rounded-t-lg last:rounded-b-lg"
           id="hs-bordered-heading-one">
@@ -91,22 +129,90 @@
               stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
             </svg>
-            Accordion #1
+            Damaged Lost Shipment
           </button>
           <div id="hs-basic-bordered-collapse-one"
             class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
             aria-labelledby="hs-bordered-heading-one">
             <div class="pb-4 px-5 ">
               <p class="text-blueNavy">
-                <em>This is the third item's accordion body.</em> It is hidden by default, until the collapse plugin
-                adds the appropriate classes that we use to style each element. These classes control the overall
-                appearance, as well as the showing and hiding via CSS transitions.
+                Apa yang terjadi jika kiriman saya rusak atau hilang?
+              <ul class="marker:text-blue-600 list-disc ps-5 space-y-2 text-sm text-gray-600 mt-3">
+                <li>
+                Jika kiriman Anda rusak atau hilang, kami akan segera menyelesaikan masalah tersebut
+                </li>
+                <li>
+                Kami akan mengganti barang yang rusak atau hilang atau memberikan kompensasi kepada Anda
+                </li>
+                <li class="">
+                  Jika Anda masih mengalami masalah, silakan hubungi tim support kami melalui link berikut
+                  <div class="flex gap-2">
+                    <p><a class="text-blue-600 hover:text-blue-500 opacity-90"
+                        href="https://wa.me/+6282229878219">Whatapp</a></p>
+                    <p><a class="text-blue-600 hover:text-blue-500 opacity-90"
+                        href="mailto:example@example.com?subject=Subject of the Email&body=Hello, this is the body of the email.">Email</a>
+                    </p>
+                  </div>
+                </li>
+              </ul>
               </p>
             </div>
           </div>
         </div>
+        {{-- 443 End --}}
+
+        {{-- Insured Shipment Delivery Start --}}
+        <div
+          class="hs-accordion active hs-accordion-active:bg-transparent border-2 -mt-px first:rounded-t-lg last:rounded-b-lg"
+          id="hs-bordered-heading-one">
+          <button
+            class="hs-accordion-toggle hs-accordion-active:bg-transparent inline-flex items-center gap-x-3 w-full font-semibold text-start text-blueNavy py-4 px-5 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none "
+            aria-controls="hs-basic-bordered-collapse-one">
+            <svg class="hs-accordion-active:hidden block size-3.5" xmlns="http://www.w3.org/2000/svg" width="24"
+              height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+              stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5v14"></path>
+            </svg>
+            <svg class="hs-accordion-active:block hidden size-3.5" xmlns="http://www.w3.org/2000/svg" width="24"
+              height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+              stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+            </svg>
+            Insured Shipment Delivery
+          </button>
+          <div id="hs-basic-bordered-collapse-one"
+            class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
+            aria-labelledby="hs-bordered-heading-one">
+            <div class="pb-4 px-5 ">
+              <p class="text-blueNavy">
+                Apakah kiriman saya diasuransikan?
+              <ul class="marker:text-blue-600 list-disc ps-5 space-y-2 text-sm text-gray-600 mt-3">
+                <li>
+                Semua kiriman kami diasuransikan secara default.
+                </li>
+                <li>
+                Anda dapat memilih untuk meningkatkan nilai asuransi jika diperlukan. <br>
+                  <p class="italic text-sm">Rata-rata durasi maintenance adalah 24 jam.</p>
+                </li>
+                <li class="">
+                  Jika Anda masih mengalami masalah, silakan hubungi tim support kami melalui link berikut
+                  <div class="flex gap-2">
+                    <p><a class="text-blue-600 hover:text-blue-500 opacity-90"
+                        href="https://wa.me/+6282229878219">Whatapp</a></p>
+                    <p><a class="text-blue-600 hover:text-blue-500 opacity-90"
+                        href="mailto:example@example.com?subject=Subject of the Email&body=Hello, this is the body of the email.">Email</a>
+                    </p>
+                  </div>
+                </li>
+              </p>
+            </div>
+          </div>
+        </div>
+        {{--Insured Shipment Delivery End --}}
       </div>
     </div>
     {{-- Accordion End --}}
   </div>
 </div>
+
